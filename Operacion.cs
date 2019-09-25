1fb07d820df0a4eb229f8e3b60cb9a2dbaaef07c
JoseCastro1800 <55769727+JoseCastro1800@users.noreply.github.com>
2019-09-25 15:35:02 -0700

    Add files via upload

---
+++ Operacion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobre_carga_de_datos
+{
+    class Operacion
+    {
+        //Aqui se agregan 3 metodos para despues llamar a la sobre carga
+        public double Multiplicar (double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Multiplicar (double a)
+        {
+            return a;
+        }
+
+        public double Multiplicar ()
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobre_carga_de_datos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aqui se instancia la clase operacion
+            Operacion OP = new Operacion();
+
+            OP.Multiplicar();
+
+            //Declaracion de variables 
+            var Rest = OP.Multiplicar(2, 3);
+            var Rest2 = OP.Multiplicar(5);
+            var Rest3 = OP.Multiplicar();
+
+            //Mandamos imprimir resultado
+
+            Console.WriteLine("El resultado 1 es igual a:" + Rest);
+            Console.WriteLine("El resultado 2 es igual a:" + Rest2);
+            Console.WriteLine("El resultado 3 es igual a:" + Rest3);
+
+            Console.ReadKey();
+            //Llamamos a la clase datos y la instanciamos
+            Datos d = new Datos ();
+            d.Obtener_Datos();
+
+        }
+    }
+}
